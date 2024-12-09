e8a8b65d44bb5e0f1290dbf1dc4a7b90c7dc0853
danshari <6458318+ezekeal@users.noreply.github.com>
2024-12-08 18:20:13 -0600

    update 8

--- 8/main.ts
+++ 8/main.ts
@@ -1,4 +1,18 @@
+type Position = [x: number, y: number];
 const [dataFile] = Deno.args;
 const data = Deno.readTextFileSync(dataFile);
+const rows = data.split('\n').map(r => r.split(''));
+const height = rows.length;
+const width = rows[0].length;
 
-console.log(data);
+const antennaLocations = new Map<string, Position>()
+const isOutOfbounds = ([x, y]: Position) => x < 0 || x > width || y < 0 || y > height;
+
+for ( const [y, row] of rows.entries() ) {
+    for (const [x, s] of row.entries()) {
+        if (s === '.') continue;
+        antennaLocations.set(s, [x, y]);
+    }
+}
+
+console.log(antennaLocations)
